6d4d1eeac33c4b67753da0dc8c0bf78af671f318
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-05-11 16:55:09 +0200

    Update name of ap in apps.py file to server.appname

--- server/testlodge/apps.py
+++ server/testlodge/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TestlodgeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'testlodge'
+    name = 'server.testlodge'
