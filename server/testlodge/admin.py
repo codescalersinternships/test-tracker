a3dcabb17866733cdc42cc28b96cd3a3bb13e2d4
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-05-11 16:54:18 +0200

    Write auto register function to register any model in testlodge app

--- server/testlodge/admin.py
+++ server/testlodge/admin.py
@@ -1,3 +1,21 @@
+from django.apps import apps
 from django.contrib import admin
 
-# Register your models here.
+
+def autoregister(*app_list : str) -> None: 
+    """
+    register all database models in the admin dashboard
+    """
+    for app in app_list :
+        for model_name, model in apps.get_app_config(app).models.items() :
+            if '_' not in model_name:
+                if globals().get(model.__name__+'Admin')   :
+                    admin.site.register(model, globals().get(model.__name__+'Admin'))
+                else :
+                    admin.site.register(model)
+
+admin.site.site_header = "Testlodge administration"
+admin.site.site_title = "Testlodge administration"
+admin.site.index_title = "Testlodge administration"
+
+autoregister('testlodge')
