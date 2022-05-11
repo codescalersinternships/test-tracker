af246d30a33f94fa6362954fc80d46a1efbb4c1b
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-05-11 21:02:47 +0200

    Rehandle the frontend side

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,30 +1,22 @@
 <script>
-	export let name;
+    import { onMount } from "svelte";
+
+    let statusCode = 200;
+
+    onMount(async () => {
+        let resp = await fetch("http://127.0.0.1:8000/api/auth/signup/").then((res) => res.json());
+        console.log(resp);
+        statusCode = resp.statusCode;
+        console.log(statusCode);
+        });
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <h1>Hello!</h1>
+    <p>
+        Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+        how to build Svelte apps.
+    </p>
+
+    <h3>Data from server</h3>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
