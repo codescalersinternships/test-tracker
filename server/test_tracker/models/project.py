a62983c3087c9e054fe5bbe2be082819c63ddd6c
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-05-16 11:55:39 +0200

    Add test run model, add some fields to test cases model

--- server/test_tracker/models/project.py
+++ server/test_tracker/models/project.py
@@ -2,12 +2,19 @@
 from django.db import models
 from server.test_tracker.models.abstracts import TimeStampedModel
 from server.test_tracker.models.dashboard import Project
+from server.test_tracker.models.users import User
 
 
 
 class PLAN_CHOICES(models.TextChoices):
     TEMPLATE = "template", "template"
     BLANK = "blank", "blank"
+
+class TEST_RUN_STATUS_CHOICES(models.TextChoices):
+    NONE = '',''
+    NOT_STARTED = "not_started", "not started"
+    IN_PROGRESS = "in_progress", "in progress"
+    COMPLETED = "completed", "completed"
 
 class TestPlan(TimeStampedModel):
     """
@@ -94,7 +101,31 @@ class TestCases(TimeStampedModel):
     test_steps = models.TextField(default="A list of steps to perform along with any sample data.")
     expected_result = models.TextField(default="Details of what the final result should be.")
     verify_requirement = models.ForeignKey(Requirements, related_name="verifies_requirements", on_delete=models.CASCADE)
+    comments = models.TextField(default="")
+    passed = models.BooleanField(default=False)
+    failed = models.BooleanField(default=False)
+    skipped = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "TestCase"
+        verbose_name_plural = "TestCases"
 
     def __str__(self):
         return self.title
+
+class TestRun(TimeStampedModel):
+    """Test run model to run all of test cases based on test suites"""
+    assigned_user = models.ForeignKey(User, related_name="assigned_user", on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    suites = models.ManyToManyField(TestSuites, related_name="run_suites")
+    status =  models.CharField(
+        max_length=100,
+        choices=TEST_RUN_STATUS_CHOICES.choices,
+        default=TEST_RUN_STATUS_CHOICES.NONE
+    )
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
 
