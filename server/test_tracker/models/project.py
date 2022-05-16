d92d2866d4b63b4e1e430efa49d7efafae9341f8
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-05-16 08:22:30 +0200

    Adding a database model for [test suites, test cases]

--- server/test_tracker/models/project.py
+++ server/test_tracker/models/project.py
@@ -73,3 +73,27 @@ class Requirements(TimeStampedModel):
 
     def __str__(self):
         return self.title
+
+class TestSuites(TimeStampedModel):
+    """
+        Class test suite model for create a new test suite for the project
+    """
+    project = models.ForeignKey(Project, related_name="project_test_suites", on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
+
+class TestCases(TimeStampedModel):
+    """
+        Class test suite model for create a new test suite for the project
+    """
+    title = models.CharField(max_length=150)
+    description = models.TextField(default="")
+    test_steps = models.TextField(default="A list of steps to perform along with any sample data.")
+    expected_result = models.TextField(default="Details of what the final result should be.")
+    verifies_requirements = models.ManyToManyField(Requirements, related_name="verifies_requirements")
+
+    def __str__(self):
+        return self.title
+
