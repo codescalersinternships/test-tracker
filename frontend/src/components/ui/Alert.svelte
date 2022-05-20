bd235863c3cb88266c51ea2b2ffa384772257c89
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-05-20 02:08:55 +0200

    Add alert componant

---
+++ frontend/src/components/ui/Alert.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let message;
+
+    function hideAlert() {
+        document.querySelector('.alert').style.display = 'none'
+    }
+</script>
+
+<section>
+    <div class="alert alert-danger text-dark" style="display: none;" role="alert">
+        <h4 class="alert-heading">Ooops!</h4>
+        <p>{message}</p>
+        <button type="button" class="btn-close" 
+            aria-label="Close" on:click={hideAlert}
+            style="position: absolute;right: 10px;top: 10px;"
+            >
+        </button>
+    </div>
+</section>
+
+
