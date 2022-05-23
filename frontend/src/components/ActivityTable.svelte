896a4be83ff813df23351f7533f16a1d54243aca
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-05-23 17:40:44 +0200

    Update activity table with project name

--- frontend/src/components/ActivityTable.svelte
+++ frontend/src/components/ActivityTable.svelte
@@ -8,34 +8,28 @@
     <div class="pt-5">
         <p>Activity</p>
     </div>
-    {#if activity}
-        <div class="table pt-4">
-            {#each activity as action}
-                <table class="table align-middle mb-3 bg-white">
-                    <thead class="bg-light">
-                        <tr>
-                            <th><span class="badge badge-success rounded-pill d-inline">{action.date}</span></th>
-                        </tr>
-                    </thead>
-                    <thead class="bg-light">
-                        <tr>
-                            <td>
-                                <div class="d-inline">
-                                    <div class="ms-3">
-                                        <p class="text-muted mb-0">{action.action}</p>
-                                    </div>
+    <div class="table pt-4">
+        {#each activity as action}
+        <table class="table align-middle mb-3 bg-white">
+                <thead class="bg-light">
+                    <tr>
+                        <th>
+                            <span class="badge badge-success rounded-pill d-inline">{action.date}</span>
+                        </th>
+                    </tr>
+                </thead>
+                <thead class="bg-light">
+                    <tr>
+                        <td>
+                            <div class="d-inline">
+                                <div class="ms-3">
+                                    <p class="text-muted mb-0">{action.action}</p>
                                 </div>
-                            </td>
-                        </tr>
-                    </thead>
-                </table>
-            {/each}
-        </div>
-    {:else}
-        <div class="col-12">
-            <p class="text-muted">
-                -- There are no activity yet
-            </p>
-        </div>
-    {/if}
+                            </div>
+                        </td>
+                    </tr>
+                </thead>
+            </table>
+        {/each}
+    </div>
 </section>
