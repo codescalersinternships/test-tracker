ddfaadf4ce217575c055c8e8e34f576bbc6edc12
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-05-23 17:44:58 +0200

    Update home page with the new refactor

--- frontend/src/pages/Home.svelte
+++ frontend/src/pages/Home.svelte
@@ -1,14 +1,16 @@
 <script>
     import { onMount } from 'svelte';
-    import axios from '../healpers/axios';
+    import axios from "../healpers/axios";
     import NavBar from "../components/NavBar.svelte";
+    import LoodingSpiner from "../components/ui/LoodingSpiner.svelte";
     import ActivityTable from "../components/ActivityTable.svelte";
     import ProjectCard from "../components/ProjectCard.svelte";
     import Search from "../components/Search.svelte";
+    import DeleteModal from "../components/ui/DeleteModal.svelte"
 
 
     export let user;
-    let projectsCopy, projects, activity;
+    let projectsCopy, projects, activity, thisProject;
     
     let config = {
         headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
@@ -22,16 +24,21 @@
         projectsCopy = projects;
     });
 
-    async function searchFunction(){
-        const projectName = document.getElementById("search-id").value;
-        if(projectName.length > 0){
-            const response = await axios.get(
-                `/project/search/${projectName}/`,config
-            )
-            projects = response.data.data
-        }else{
-            projects = projectsCopy
-        }
+    async function handleDelete(event) {
+        const project = event.detail.obj;
+        const indx = projects.findIndex(v => v.id === project.id);
+        projects = projects;
+        projects.splice(indx, 1);
+    }
+
+    async function handleSearch(event) {
+        const searchProjects = event.detail.objects;
+        projects = searchProjects;
+    }
+
+    function setProject(project) {
+        thisProject = project
+        document.querySelector('.modal').style.display = 'block'
     }
 
 </script>
@@ -41,31 +48,49 @@
 </svelte:head>
 
 <section>
-    {#if user && projects}
+    {#if user}
         <NavBar user={user} />
         <div class="container pt-4">
-            You are <strong>{user.permission}</strong> of 
-            <strong>{user.projects}</strong>
-            {user.projects === 1 ? 'project' : 'projects'}
-
-            <Search title="Search Projects" searchFunction={searchFunction}/>
-            <div class="pt-5">
-                <p class="last-projects">Last <strong>{user.projects}</strong>
-                    {user.projects === 1 ? 'Project' : 'Projects'} Updated</p>
-                <div class="row p-1">
-                    {#each projects as project}
-                        <ProjectCard title={project.name.length > 25 ? project.name.slice(0,25)+'..' : project.name} 
-                            date={project.created} link={`/projects/${project.id}`}/>
-                    {:else}
-                        <div class="col-12 last-projects-notfound pt-3">
-                            <p class="text-muted">
-                                -- There are no projects yet
-                            </p>
-                        </div>
-                    {/each}
+            {#if projects}
+                {#if user.permission !== "admin"}
+                    You are <strong>{user.permission}</strong> of 
+                    <strong>{user.projects}</strong>
+                    {user.projects === 1 ? 'project' : 'projects'}
+                {/if}
+                <div class="pt-4">
+                    <p>
+                        Search Projects
+                    </p>
+                    <Search request="/project/search/" objects={projects} config={config} objectsCopy={projectsCopy} on:message={handleSearch}/>
                 </div>
-            </div>   
-            <ActivityTable activity={activity}/>
+                <div class="pt-5">
+                    {#if projects.length > 0}
+                        <p class="last-projects">
+                            Last <strong>{projects.length}</strong>
+                            {projects.length === 1 ? 'Project' : 'Projects'} Updated
+                        </p>
+                    {/if}
+                    <div class="row p-1">
+                        {#each projects as project}
+                            <ProjectCard project={project} link={`/projects/${project.id}`}>
+                                <button class="dropdown-item text-danger" on:click={setProject(project)}>Delete</button>
+                            </ProjectCard>
+                        {/each}
+                    </div>
+                </div>
+                {#if activity && activity.length > 0}
+                    <ActivityTable activity={activity}/>
+                {/if}
+            {/if}
         </div>
+    {:else}
+        <LoodingSpiner />
     {/if}
+    
+    <DeleteModal
+        on:message={handleDelete}
+        obj={thisProject}
+        onRequest='/project'
+        config={config}
+    />
 </section>
