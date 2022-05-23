3f6ab9a2a6295bff265e1cb19ac48f7f0bba2367
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-05-23 17:44:25 +0200

    Add some refactor on search componant to make it work on any search form

--- frontend/src/components/Search.svelte
+++ frontend/src/components/Search.svelte
@@ -1,22 +1,37 @@
-<script>    
-    export let title;
-    export let searchFunction;
+<script>
+    import { createEventDispatcher } from 'svelte';
+    import axios from "../healpers/axios";
+
+    export let objects; // [objects] is the array of objects to update with the new data
+    export let objectsCopy; // [objectsCopy] is a copy of the objects array
+    export let request; // [request] for the endpoint
+    export let config; // [config] for the headers
+
+    const dispatch = createEventDispatcher();
+    let searchInput;
+
+    async function searchFunction(){
+        const word = searchInput.value;
+        if(word.length > 0){
+            const response = await axios.get(
+                `${request}${word}/`,config
+            )
+            objects = response.data.data
+        }else{
+            objects = objectsCopy
+        }
+        dispatch('message', {
+            objects: objects
+        });
+    }
+
 </script>
 
-
-
-<section>
-    <div class="pt-4">
-        <p>
-            {title}
-        </p>
-        <div class="input-group">
-            <input id="search-id" type="search" class="form-control rounded" placeholder="Search"
-            aria-label="Search" aria-describedby="search-addon" />
-            <button type="button" class="btn btn-outline-primary" 
-                on:click={searchFunction}>
-                Search
-            </button>
-        </div>
-    </div>
-</section>
+<div class="input-group">
+    <input bind:this={searchInput} type="search" class="form-control rounded" placeholder="Search"
+    aria-label="Search" aria-describedby="search-addon" />
+    <button type="button" class="btn btn-outline-primary" 
+        on:click={searchFunction}>
+        Search
+    </button>
+</div>
