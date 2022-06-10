ca51a27f336a18f11fb5fc93d61413b96e05bbe7
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-06-10 16:39:41 +0200

    Defined an new componant that named MemberCard to use it with members, project team section pages

---
+++ frontend/src/components/MemberCard.svelte
@@ -0,0 +1,74 @@
+<script>
+    import Dropdown from "../components/ui/Dropdown.svelte";
+    import { Router, Link } from "svelte-navigator";
+
+    export let member;
+</script>
+
+<div class="col-12 mb-4">
+    <div class="card">
+        <div class="coldrop">
+            <Dropdown>
+                <Router>
+                    <li>
+                        <slot></slot>
+                    </li>
+                </Router>
+            </Dropdown>
+        </div>
+        <Link
+            to="/members/{member.id}/"
+            class="text-dark d-block text-decoration-none"
+        >
+            <div
+                class="card-body d-flex align-items-center"
+            >
+                <span class="user_photo">
+                    {member.first_name[0]}{member
+                        .last_name[0]}
+                </span>
+                <div class="info_user">
+                    <strong
+                        >{member.full_name}</strong
+                    >
+                    <p class="text-muted mb-0">
+                        Member since: {member.created}
+                    </p>
+                </div>
+            </div>
+        </Link>
+    </div>
+</div>
+
+<svelte:head>
+    <style>
+        .coldrop{
+            position: absolute;
+            font-size: 0;
+            right: 0; 
+            top: 20px;
+        }
+        .user_photo {
+            display: inline-block;
+            background: #5a79b1;
+            margin-right: 15px;
+            width: 60px;
+            height: 60px;
+            border-radius: 50%;
+            line-height: 60px;
+            text-align: center;
+            font-weight: 900;
+            color: #fff;
+        }
+        .info_user {
+            flex: 1;
+        }
+
+        .info_user strong {
+            font-size: 20px;
+            font-weight: bold;
+            margin-bottom: 10px;
+            color: #5a79b1;
+        }
+    </style>
+</svelte:head>
--- frontend/src/pages/Members.svelte
+++ frontend/src/pages/Members.svelte
@@ -1,13 +1,13 @@
 <script>
     import { onMount } from "svelte";
     import { Link } from "svelte-navigator";
-
     import axios from "../healpers/axios";
 
     import NavBar from "../components/NavBar.svelte";
     import Search from "../components/Search.svelte";
     import LoodingSpiner from "../components/ui/LoodingSpiner.svelte";
     import DeleteModal from "../components/ui/DeleteModal.svelte";
+    import MemberCard from "../components/MemberCard.svelte";
 
     export let user;
 
@@ -50,30 +50,6 @@
 
 <svelte:head>
     <title>Test-Tracker | Members</title>
-    <style>
-        .user_photo {
-            display: inline-block;
-            background: #5a79b1;
-            margin-right: 15px;
-            width: 60px;
-            height: 60px;
-            border-radius: 50%;
-            line-height: 60px;
-            text-align: center;
-            font-weight: 900;
-            color: #fff;
-        }
-        .info_user {
-            flex: 1;
-        }
-
-        .info_user strong {
-            font-size: 20px;
-            font-weight: bold;
-            margin-bottom: 10px;
-            color: #5a79b1;
-        }
-    </style>
 </svelte:head>
 
 <section>
@@ -100,67 +76,14 @@
                 <div class="pt-5">
                     <div class="row">
                         {#each members as member}
-                            <div class="col-12 mb-4">
-                                <div class="card">
-                                    <div
-                                        class="dropdown p-1"
-                                        style="position: absolute;font-size: 0;right: 0; top: 20px;"
-                                    >
-                                        <a
-                                            class="dropdown-toggle"
-                                            id="dropdownMenuButton"
-                                            data-mdb-toggle="dropdown"
-                                            aria-expanded="false"
-                                        >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width="20"
-                                                height="20"
-                                                fill="currentColor"
-                                                class="bi bi-three-dots-vertical"
-                                                viewBox="0 0 16 16"
-                                            >
-                                                <path
-                                                    d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"
-                                                />
-                                            </svg>
-                                        </a>
-                                        <ul
-                                            class="dropdown-menu"
-                                            aria-labelledby="dropdownMenuButton"
-                                        >
-                                            <li>
-                                                <button
-                                                    class="dropdown-item text-danger"
-                                                    on:click={setMember(member)}
-                                                    >Delete</button
-                                                >
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <Link
-                                        to="/members/{member.id}/"
-                                        class="text-dark d-block text-decoration-none"
-                                    >
-                                        <div
-                                            class="card-body d-flex align-items-center"
-                                        >
-                                            <span class="user_photo">
-                                                {member.first_name[0]}{member
-                                                    .last_name[0]}
-                                            </span>
-                                            <div class="info_user">
-                                                <strong
-                                                    >{member.full_name}</strong
-                                                >
-                                                <p class="text-muted mb-0">
-                                                    Member since: {member.created}
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
+                            <MemberCard {member}>
+                                <Link 
+                                    to=""
+                                    class="dropdown-item text-danger" 
+                                    on:click={setMember(member)}
+                                    >Delete {member.first_name}
+                                </Link>
+                            </MemberCard>
                         {/each}
                     </div>
                 </div>
