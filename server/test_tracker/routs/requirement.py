9430607178512e809e34ce9980f1e5e97361c45d
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-06-16 11:24:44 +0200

    improve requirements routs

--- server/test_tracker/routs/requirement.py
+++ server/test_tracker/routs/requirement.py
@@ -6,16 +6,16 @@ from server.test_tracker.views.requirement import *
 
 
 urlpatterns = [
-    path('projects/<str:project_id>/get-all/', GetAllRequirementDocssAPIView.as_view()),
+    path('projects/<str:project_id>/get-all/', GetAllRequirementDocsAPIView.as_view()),
     path('projects/<str:project_id>/search/<str:key_word>/', SearchRequirementDocssAPIView.as_view()),
     path('projects/<str:project_id>/search/requirements/<str:key_word>/', SearchRequirementsInRequirementDocssAPIView.as_view()),
     path('projects/<str:project_id>/details/<str:requirement_id>/', 
-        RequirementDocssDetailsAPIView.as_view()
+        RequirementDocsDetailsAPIView.as_view()
     ),
 
-    path('projects/<str:project_id>/post/', PostNewRequirementDocssAPIView.as_view()),
+    path('<str:project_id>/', PostNewRequirementDocsAPIView.as_view()),
 
-    path('<str:requirements_id>/', RequirementAPIView.as_view()),
+    path('projects/<str:project_id>/requirement/<str:requirements_id>/', RequirementAPIView.as_view()),
     path('projects/<str:project_id>/<str:project_requirement_id>/<str:requirement_id>/', 
         RequirementsDetailAPIView.as_view()
     ),
