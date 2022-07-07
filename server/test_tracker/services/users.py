b66b9c2af086feb205ee2b3f1c64e39bb31d9278
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-07-07 12:26:43 +0200

    Remove  function from user healper method

--- server/test_tracker/services/users.py
+++ server/test_tracker/services/users.py
@@ -5,12 +5,10 @@ from django.contrib.auth.hashers import check_password
 
 def get_user_by_id(id: str) -> User:
     """Return user who have the same id"""
-    if id.isdigit():
-        try:
-            return User.objects.get(id=int(id))
-        except:
-            return None
-    return None
+    try:
+        return User.objects.get(id=int(id))
+    except:
+        return None
 
 def get_user_by_email_for_login(email: str) -> User:
     """Return user who have the same email"""
