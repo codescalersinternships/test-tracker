6ae966fbe46acd05ebf8e08855ee7e757937f665
mayar osama <mayarosama18@gmail.com>
2024-07-22 16:16:01 +0300

    Removing old client

--- client/src/healpers/theme.js
+++
@@ -1,9 +0,0 @@
-export function setTheme(mode){
-    if(mode === "light"){
-        window.document.body.classList.add('light');
-        window.document.body.classList.remove('dark');
-    } else if(mode === "dark"){
-        window.document.body.classList.add('dark');
-        window.document.body.classList.remove('light');
-    }
-}
