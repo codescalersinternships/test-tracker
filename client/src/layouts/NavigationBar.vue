eb51f216d223e8f7f5469085d9266814c10337f1
abdahmed22 <abd.ahmed03@eng-st.cu.edu.eg>
2024-08-01 16:00:38 +0300

    update: validation ranges & create computed props for routes

--- client/src/layouts/NavigationBar.vue
+++ client/src/layouts/NavigationBar.vue
@@ -1,56 +1,53 @@
 <template>
   <v-app-bar
+    class="px-9"
     color="teal-darken-4"
   >
-    <v-spacer />
-    <RouterLink
-      v-for="(item,index) in createMainRoutes(routeStore.routeName)"
-      :key="index"
-      class="mx-3 text-white bg-teal-darken-3 pa-3 rounded-lg font-weight-black"
-      :to="{ name: item.routeName}"
-    >
-      {{ item.displayName }}
-    </RouterLink>
+    <div class="app-component mx-9">
+      <RouterLink
+        v-for="(item,index) in mainRoutes"
+        :key="index"
+        class="mx-3 text-white bg-teal-darken-3 pa-3 rounded-lg font-weight-black"
+        :to="{ name: item.routeName}"
+      >
+        {{ item.displayName }}
+      </RouterLink>
+    </div>
 
-    <v-spacer />
-    <v-spacer />
-    <v-spacer />
-    <v-spacer />
-    <v-spacer />
+    <div class="app-component mx-9">
+      <v-menu transition="fab-transition">
+        <template #activator="{ props }">
+          <v-btn v-if="displayMenuRoutes(routeStore.routeName)" icon="mdi-plus" size="large" v-bind="props" />
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(item,index) in menuRoutes"
+            :key="index"
+          >
+            <RouterLink class="mx-3 text-white pa-3 font-weight-black" :to="{ name: item.routeName}">
+              {{ item.displayName }}
+            </RouterLink>
+          </v-list-item>
+        </v-list>
+      </v-menu>
 
-    <v-menu transition="fab-transition">
-      <template #activator="{ props }">
-        <v-btn v-if="displayMenuRoutes(routeStore.routeName)" icon="mdi-plus" size="large" v-bind="props" />
-      </template>
-      <v-list>
-        <v-list-item
-          v-for="(item,index) in createMenuRoutes(routeStore.routeName)"
-          :key="index"
-        >
-          <RouterLink class="mx-3 text-white pa-3 font-weight-black" :to="{ name: item.routeName}">
-            {{ item.displayName }}
-          </RouterLink>
-        </v-list-item>
-      </v-list>
-    </v-menu>
+      <v-menu transition="fab-transition">
+        <template #activator="{ props }">
+          <v-btn icon="mdi-account" size="large" v-bind="props" />
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(item,index) in profileRoutes"
+            :key="index"
+          >
+            <RouterLink class="mx-3 text-white pa-3 font-weight-black" :to="{ name: item.routeName}">
+              {{ item.displayName }}
+            </RouterLink>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
 
-    <v-menu transition="fab-transition">
-      <template #activator="{ props }">
-        <v-btn icon="mdi-account" size="large" v-bind="props" />
-      </template>
-      <v-list>
-        <v-list-item
-          v-for="(item,index) in profileRoutes"
-          :key="index"
-        >
-          <RouterLink class="mx-3 text-white pa-3 font-weight-black" :to="{ name: item.routeName}">
-            {{ item.displayName }}
-          </RouterLink>
-        </v-list-item>
-      </v-list>
-    </v-menu>
-
-    <v-spacer />
   </v-app-bar>
 </template>
 
@@ -83,134 +80,6 @@
         ]
       )
 
-      function createMainRoutes (routeName: string): AppRoute[] {
-        const mainRoutes: AppRoute[] =
-          [
-            {
-              displayName: 'Dashboard',
-              routeName: 'dashboard',
-            },
-            {
-              displayName: 'Members',
-              routeName: 'members',
-            },
-            {
-              displayName: 'Projects',
-              routeName: 'projects',
-            },
-          ]
-
-        if (routeName === 'dashboard') {
-          mainRoutes.push(
-            {
-              displayName: 'Settings',
-              routeName: 'settings',
-            },
-          )
-        } else if (routeName === 'projectDetails') {
-          mainRoutes.push(
-            {
-              displayName: 'Test Plan',
-              routeName: 'testPlan',
-            },
-            {
-              displayName: 'Requirment',
-              routeName: 'testRequirment',
-            },
-            {
-              displayName: 'Test Suite',
-              routeName: 'testSuite',
-            },
-            {
-              displayName: 'Test Run',
-              routeName: 'testRun',
-            },
-          )
-        }
-        return mainRoutes
-      }
-
-      function createMenuRoutes (routeName: string): AppRoute[] {
-        const menuRoutes: AppRoute[] = []
-
-        if (routeName === 'projects') {
-          menuRoutes.push(
-            {
-              displayName: 'New Project',
-              routeName: 'newProject',
-            }
-          )
-        }
-
-        if (routeName === 'projectDetails') {
-          menuRoutes.push(
-            {
-              displayName: 'New Test Plan',
-              routeName: 'newTestPlan',
-            },
-            {
-              displayName: 'New Requirment',
-              routeName: 'newRequirment',
-            },
-            {
-              displayName: 'New Test Suite',
-              routeName: 'newTestSuite',
-            },
-            {
-              displayName: 'New Test Run',
-              routeName: 'newTestRun',
-            },
-          )
-        }
-
-        if (routeName === 'testPlan') {
-          menuRoutes.push(
-            {
-              displayName: 'New Test Plan',
-              routeName: 'newTestPlan',
-            },
-          )
-        }
-
-        if (routeName === 'testRequirment') {
-          menuRoutes.push(
-            {
-              displayName: 'New Requirment',
-              routeName: 'newRequirment',
-            },
-          )
-        }
-
-        if (routeName === 'testSuite') {
-          menuRoutes.push(
-            {
-              displayName: 'New Test Suite',
-              routeName: 'newTestSuite',
-            },
-          )
-        }
-
-        if (routeName === 'testRun') {
-          menuRoutes.push(
-            {
-              displayName: 'New Test Run',
-              routeName: 'newTestRun',
-            },
-          )
-        }
-
-        if (routeName === 'members') {
-          menuRoutes.push(
-            {
-              displayName: 'Invite Member',
-              routeName: 'newMember',
-            },
-          )
-        }
-
-        return menuRoutes
-      }
-
       function displayMenuRoutes (routeName: string): boolean {
         if (routeName === 'dashboard') {
           return false
@@ -226,11 +95,160 @@
       return {
         profileRoutes,
         routeStore,
-        createMenuRoutes,
-        createMainRoutes,
         displayMenuRoutes,
         RouterLink,
       }
+    },
+    computed: {
+      mainRoutes () {
+        const routeName = this.routeStore.routeName
+
+        if (routeName === 'dashboard' | routeName === 'settings') {
+          return [
+            {
+              displayName: 'Dashboard',
+              routeName: 'dashboard',
+            },
+            {
+              displayName: 'Members',
+              routeName: 'members',
+            },
+            {
+              displayName: 'Projects',
+              routeName: 'projects',
+            },
+            {
+              displayName: 'Settings',
+              routeName: 'settings',
+            },
+          ]
+        } else if (routeName === 'projectDetails') {
+          return [
+            {
+              displayName: 'Dashboard',
+              routeName: 'dashboard',
+            },
+            {
+              displayName: 'Members',
+              routeName: 'members',
+            },
+            {
+              displayName: 'Projects',
+              routeName: 'projects',
+            },
+            {
+              displayName: 'Test Plan',
+              routeName: 'testPlan',
+            },
+            {
+              displayName: 'Requirement',
+              routeName: 'testRequirment',
+            },
+            {
+              displayName: 'Test Suite',
+              routeName: 'testSuite',
+            },
+            {
+              displayName: 'Test Run',
+              routeName: 'testRun',
+            },
+          ]
+        }
+        return [
+          {
+            displayName: 'Dashboard',
+            routeName: 'dashboard',
+          },
+          {
+            displayName: 'Members',
+            routeName: 'members',
+          },
+          {
+            displayName: 'Projects',
+            routeName: 'projects',
+          },
+        ]
+      },
+      menuRoutes () {
+        const routeName = this.routeStore.routeName
+
+        if (routeName === 'projects') {
+          return [
+            {
+              displayName: 'New Project',
+              routeName: 'newProject',
+            },
+          ]
+        }
+
+        if (routeName === 'projectDetails') {
+          return [
+            {
+              displayName: 'New Test Plan',
+              routeName: 'newTestPlan',
+            },
+            {
+              displayName: 'New Requirment',
+              routeName: 'newRequirment',
+            },
+            {
+              displayName: 'New Test Suite',
+              routeName: 'newTestSuite',
+            },
+            {
+              displayName: 'New Test Run',
+              routeName: 'newTestRun',
+            },
+          ]
+        }
+
+        if (routeName === 'testPlan') {
+          return [
+            {
+              displayName: 'New Test Plan',
+              routeName: 'newTestPlan',
+            },
+          ]
+        }
+
+        if (routeName === 'testRequirment') {
+          return [
+            {
+              displayName: 'New Requirment',
+              routeName: 'newRequirment',
+            },
+          ]
+        }
+
+        if (routeName === 'testSuite') {
+          return [
+            {
+              displayName: 'New Test Suite',
+              routeName: 'newTestSuite',
+            },
+          ]
+        }
+
+        if (routeName === 'testRun') {
+          return [
+            {
+              displayName: 'New Test Run',
+              routeName: 'newTestRun',
+            },
+          ]
+        }
+
+        if (routeName === 'members') {
+          return [
+            {
+              displayName: 'Invite Member',
+              routeName: 'newMember',
+            },
+          ]
+        }
+
+        return []
+      },
     },
   }
 </script>
@@ -241,6 +259,11 @@ a {
   text-decoration: none;
   color: inherit;
   font-size: 0.75rem;
+}
+
+::v-deep .v-toolbar__content {
+  display: flex !important;
+  justify-content: space-between !important;
 }
 
 </style>
--- client/src/utilities/validators.ts
+++ client/src/utilities/validators.ts
@@ -1,21 +1,21 @@
 export const passwordRules = [
   (value: string) => {
-    if (value?.length > 1) return true
-    return 'Password must be at least 1 character.'
+    if (value?.length > 3) return true
+    return 'Password must be at least 4 character.'
   },
   (value: string) => {
-    if (value?.length < 129) return true
+    if (value?.length < 127) return true
     return 'Password must be at most 128 characters.'
   },
 ]
 
 export const nameRules = [
   (value: string) => {
-    if (value?.length > 1) return true
-    return 'Name must be at least 1 character.'
+    if (value?.length > 4) return true
+    return 'Name must be at least 5 character.'
   },
   (value: string) => {
-    if (value?.length < 51) return true
+    if (value?.length < 49) return true
     return 'Name must be at most 50 characters.'
   },
   (value: string) => {
@@ -26,11 +26,11 @@ export const nameRules = [
 
 export const phoneNumberRules = [
   (value: string) => {
-    if (value?.length < 21) return true
+    if (value?.length < 19) return true
     return 'Phone number must be at most 20 digits.'
   },
   (value: string) => {
-    if (/[^0-9]/.test(value)) return 'Name can not contain characters.'
+    if (/[^0-9]/.test(value)) return 'Phone number can not contain characters.'
     return true
   },
 ]
