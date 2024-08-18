45ce05a840c553bd125dc4e6cc180249cbcb2c33
nabilasherif <nabila.sherif81@gmail.com>
2024-08-18 16:12:16 +0300

    get members, added props, notifier

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,27 +1,23 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance } from 'axios'
 
 const AuthClient: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_APP_ENDPOINT, 
+  baseURL: import.meta.env.VITE_APP_ENDPOINT,
   timeout: 1000,
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + localStorage.getItem("token"),
+    Authorization: 'Bearer ' + localStorage.getItem('token'),
   },
-});
+})
 
 const BaseClient: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_APP_ENDPOINT,
   timeout: 1000,
-});
+})
 
-async function Search(searchInput:any) {
-  await AuthClient.get(`/members/search/${searchInput}`)
-  .then(response=>{
-    return response.data;
-  })
-  .catch(error=>{
-    console.error(error);
-  })
+async function Search (searchInput:any) {
+  return await AuthClient.get(`/members/search/${searchInput}`)
 }
-
-export default{ AuthClient, BaseClient, Search };
+async function getProjectMembers (projectId:any) {
+  return await AuthClient.get(`/members/project/${projectId}/members/`)
+}
+export default { AuthClient, BaseClient, Search }
--- client/src/pages/AddMembers.vue
+++ client/src/pages/AddMembers.vue
@@ -1,140 +1,191 @@
 <template>
-    <div style="margin-left: 7cm; margin-right: 7cm;">
-        <v-container>
-            <v-row >
-                <p class="mt-2 text-h4 text-blue-darken-4" variant="h5">ALL MEMBERS</p>
-                <v-spacer></v-spacer>
-                <v-btn color="primary" @click="addMemberDialog=true">
-                    INVITE MEMBERS
-                </v-btn>
-            </v-row>
+  <div style="margin-left: 7cm; margin-right: 7cm;">
+    <v-container>
+      <v-row>
+        <p class="mt-2 text-h4 text-blue-darken-4" variant="h5">ALL MEMBERS</p>
+        <v-spacer />
+        <v-btn
+          color="primary"
+          @click="addMemberDialog=true"
+        >
+          INVITE MEMBERS
+        </v-btn>
+      </v-row>
 
-            <v-dialog v-model="addMemberDialog" max-width="600px">
-                <v-card>
-                    <v-card-title>
-                        <br>
-                        <v-divider></v-divider>
-                    </v-card-title>
-                    <v-card-text>
-                        <p>First Name</p>
-                        <v-text-field
-                        v-model="inviteNewMember.first_name"
-                        density="compact"
-                        placeholder="First Name"
-                        prepend-inner-icon="mdi-account-outline"
-                        variant="outlined"
-                        ></v-text-field>
-                        <p>Last Name</p>
-                        <v-text-field
-                        v-model="inviteNewMember.last_name"
-                        density="compact"
-                        placeholder="Last Name"
-                        prepend-inner-icon="mdi-account-outline"
-                        variant="outlined"
-                        ></v-text-field>
-                        <p>Email</p>
-                        <v-text-field
-                        v-model="inviteNewMember.email"
-                        density="compact"
-                        placeholder="Email"
-                        prepend-inner-icon="mdi-email-outline"
-                        variant="outlined"
-                        ></v-text-field>
-                        <p>Permission</p>
-                        <v-select
-                        label="Select"
-                        :items="['Full access', 'Admin access']"
-                        ></v-select>
-                        <v-divider></v-divider>
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn color="info" @click="addMemberDialog = false">Close</v-btn>
-                            <v-btn color="success" @click="addMember">ADD+</v-btn>
-                        </v-card-actions>
-                    </v-card-text>
+      <v-dialog v-model="addMemberDialog" max-width="600px">
+        <v-card>
+          <v-form>
+            <v-card-title>
+              <br>
+              <v-divider />
+            </v-card-title>
+            <v-card-text>
+              <v-text-field
+                v-model="inviteNewMember.first_name"
+                density="compact"
+                placeholder="First Name"
+                prepend-inner-icon="mdi-account-outline"
+                variant="outlined"
+              />
+              <v-text-field
+                v-model="inviteNewMember.last_name"
+                density="compact"
+                placeholder="Last Name"
+                prepend-inner-icon="mdi-account-outline"
+                variant="outlined"
+              />
+              <v-text-field
+                v-model="inviteNewMember.email"
+                density="compact"
+                placeholder="Email"
+                prepend-inner-icon="mdi-email-outline"
+                variant="outlined"
+              />
+              <p>Permission</p>
+              <v-select
+                :items="['Full access', 'Admin access']"
+                label="Select"
+              />
+              <v-divider />
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="info" @click="addMemberDialog = false">Close</v-btn>
+                <v-btn color="success" :disabled="loadingAdd" :loading="loadingAdd" @click="addMember">ADD+</v-btn>
+              </v-card-actions>
+            </v-card-text>
+          </v-form>
+        </v-card>
+      </v-dialog>
 
-                </v-card>
-            </v-dialog>
+      <v-row>
+        <p class="mt-2 text-h6 text-grey-darken-2 mb-8" variant="h6">There are {{ count }} members registered</p>
+      </v-row>
+      <br>
 
-            <v-row>
-                <p class="mt-2 text-h6 text-grey-darken-2 mb-8" variant="h6">There are {{ count }} members registered</p>
-            </v-row>
-            <br>
+      <v-row>
+        <h4 class="mb-2">Search Members</h4>
+      </v-row>
 
-            <v-row>
-                <h4  class="mb-2">Search Members</h4>
-            </v-row>
+      <v-row>
+        <v-text-field v-model="searchText" label="Search" variant="outlined" />
+        <v-btn color="primary" style="height: 56px; width:80px; padding: 0; margin: 0;" variant="outlined" @click="SearchMember">Search</v-btn>
+      </v-row>
 
-            <v-row>
-                <v-text-field v-model="searchText" label="Search" variant="outlined"></v-text-field>
-                <v-btn color="primary" variant="outlined" @click="SearchMember"  style="height: 56px; width:80px; padding: 0; margin: 0;">Search</v-btn>
-            </v-row>
-
-                    <!-- seperate component for displaying members? -->
-            <v-row>
-                <v-col v-for="member in members" :key="member.email" cols="12" sm="6" md="4">
-                    <v-card class="ma-2" outlined>
-                        <!-- <v-card-title>{{ member.full_name }}</v-card-title> -->
-                        <v-card-subtitle>{{ member.full_name }}</v-card-subtitle>
-                        <v-card-actions>
-                        <v-btn @click="viewMember(member.id)">View Details</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-col>
-        </v-row>
-        </v-container>
-    </div>
+      <v-row>
+        <v-col
+          v-for="member in members"
+          :key="member.email"
+          cols="12"
+          md="4"
+          sm="6"
+        >
+          <v-card class="ma-2" outlined>
+            <!-- implement viewMember -->
+            <v-card-subtitle>{{ member.full_name }}</v-card-subtitle>
+            <v-card-actions>
+              <v-btn @click="viewMember(member.id)">View Details</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue';
-import { defineProps } from 'vue';
-import axios from '@/api/axios';
+  import { defineProps, ref } from 'vue'
+  import axios from '@/api/axios'
 
-export default{
-    setup(){
-        const props =defineProps({
-            count:{
-                type :Number,
-                required : true,
-            },
-            members:{
-                type :Array,
-                required :true, 
-            },
-        });
+  export default {
+    setup () {
+      const props = defineProps({
+        count: {
+          type: Number,
+          required: true,
+        },
+        members: {
+          type: Array,
+          required: true,
+        },
+        project_id: {
+          type: string,
+          required: true,
+        },
+      })
 
-        const inviteNewMember=ref({
-        first_name: "",
-        last_name: "",
-        email:"",
-        })
+      const inviteNewMember = ref({
+        first_name: '',
+        last_name: '',
+        email: '',
+      })
 
-        const addMemberDialog=ref(false);
+      const addMemberDialog = ref(false)
 
-        let searchText=ref("");
+      const searchText = ref('')
 
-        const addMember = () => {
-            // take userinfo
-        };
+      const loadingAdd = ref(false)
 
-        const SearchMember = async () => {
-            try {
-               members.value= await axios.Search(searchText.value);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        return{
-            props,
-            SearchMember,
-            searchText,
-            inviteNewMember,
-            addMemberDialog,
-            addMember
+      const addMember = () => {
+        loadingAdd.value = true
+        try {
+          notifier.notify({
+            title: 'success',
+            description: 'member added successfully',
+            showProgressBar: true,
+            timeout: 7_000,
+            type: 'success',
+          })
+        } catch (error) {
+          console.error(error)
+          notifier.notify({
+            title: 'Fail',
+            description: 'cannot add member',
+            showProgressBar: true,
+            timeout: 7_000,
+            type: 'error',
+          })
+        } finally {
+          loadingAdd.value = false
         }
-    }
-    
-}
+      }
+
+      const SearchMember = async () => {
+        try {
+          members.value = await axios.Search(searchText.value)
+        } catch (error) {
+          console.error(error)
+          notifier.notify({
+            title: 'Fail',
+            description: 'no member found',
+            showProgressBar: true,
+            timeout: 7_000,
+            type: 'error',
+          })
+        }
+      }
+
+      async function getMembers () {
+        try {
+          members = await axios.getProjectMembers(project_id)
+        } catch (error) {
+          console.error(error)
+        }
+      }
+
+      onMounted(() => {
+        getMembers()
+      })
+
+      return {
+        props,
+        SearchMember,
+        searchText,
+        inviteNewMember,
+        addMemberDialog,
+        addMember,
+        loadingAdd,
+      }
+    },
+
+  }
 </script>
