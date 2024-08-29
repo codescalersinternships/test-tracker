bb7a581997ba690d9104a1d9c2d387873c96515b
Nabila Sherif <161375519+nabilasherif@users.noreply.github.com>
2024-08-29 17:43:42 +0300

    Update axios.ts

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -2,9 +2,7 @@ import axiosClient from 'axios'
 
 const accessToken = localStorage.getItem('TESTTRACKER_ACCESS_TOKEN')
 export const axios = axiosClient.create({
-
-  // window.env.SERVER_DOMAIN_NAME_API
-  baseURL: 'https://server.gent02.dev.grid.tf',
+  baseURL: window.env.SERVER_DOMAIN_NAME_API,
   headers: {
     Authorization: accessToken ? `Bearer ${accessToken}` : '',
   },
